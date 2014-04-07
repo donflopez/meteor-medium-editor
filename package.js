e71246dcf292f5fea86fc8d3f82e98cb3e33ab1d
Francisco López <donflopez@gmail.com>
2014-04-07 18:59:28 +0200

    Added medium editor insert plugin

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
 	name: "donflopez:medium-editor",
-	summary: "Add medium-editor clone to Meteor client side",
-	version: "1.0.1"
+	summary: "Add medium-editor and medium insert clone to Meteor client side",
+	version: "1.1.0"
 });
 
 Package.on_use(function (api) {
-	api.add_files(['medium-editor.css','medium-editor.min.js'], 'client');
+	api.add_files(['medium-editor.css','medium-editor.min.js', 'medium-editor-insert-plugin.all.min.js', 'medium-editor-insert-plugin.css'], 'client');
 });
