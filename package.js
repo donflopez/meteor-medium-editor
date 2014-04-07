37155b760c848fa58b582de0f02c2bb4a68dbd16
Francisco López <donflopez@gmail.com>
2014-04-07 18:30:16 +0200

    Initial commit

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+	name: "donflopez:medium-editor",
+	summary: "Add medium-editor clone to Meteor client side",
+	version: "1.0.0"
+});
+
+Package.on_use(function (api) {
+	api.add_files(['medium-editor.css','medium-editor.min.js'], 'client');
+});
