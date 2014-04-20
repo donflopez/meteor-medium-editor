6d2f34b560b2682341aae23fc78251ccea504cd8
Francisco López <donflopez@gmail.com>
2014-04-20 15:15:22 +0200

    Bugfix

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
 	name: "donflopez:medium-editor",
 	summary: "Add medium-editor and medium insert clone to Meteor client side",
-	version: "1.1.0"
+	version: "1.2.0"
 });
 
 Package.on_use(function (api) {
-	api.add_files(['medium-editor.css','medium-editor.min.js', 'medium-editor-insert-plugin.all.min.js', 'medium-editor-insert-plugin.css'], 'client');
+	api.add_files(['medium-editor.css','medium-editor.js', 'medium-editor-insert-plugin.all.min.js','medium-editor-insert-plugin.css'], 'client');
+
+	if(api.export)
+		api.export('MediumEditor', 'client');
 });
